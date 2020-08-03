148858b731e9a4c3641e897092a338efbdbda2a8
Diana Kulich <diana.kulich@oxagile.com>
2020-08-03 13:59:56 +0300

    EvenVizion component. Version 1.0

---
+++ compare_evenvizion_with_original_video.py
@@ -0,0 +1,90 @@
+"""
+stitch two visualization (compare real video and EvenVizion)
+"""
+import argparse
+import logging
+import os
+
+import cv2
+import imutils
+import numpy as np
+
+logging.basicConfig(level=logging.INFO)
+
+
+def create_border(image_b):
+    """
+    create image border
+    """
+    pt1 = (0, 0)
+    pt2 = (image_b.shape[1] - 1, image_b.shape[0] - 1)
+    image_b = cv2.rectangle(image_b, pt1, pt2, color=[0, 0, 248], thickness=1, lineType=8, shift=0)
+    return image_b
+
+
+def image_design(image_path, text, height=300):
+    """
+    put text on frame
+    """
+    if text == "EvenVizion":
+        image = cv2.imread(image_path)
+    elif text == "Original":
+        image = image_path
+    else:
+        raise ValueError("problem with type in image_design")
+    image = imutils.resize(image, height=height)
+    image = cv2.putText(image, text, (10, 20),
+                        fontFace=cv2.FONT_HERSHEY_PLAIN, color=(255, 255, 255),
+                        fontScale=1, thickness=1)
+    return image
+
+
+def get_pictures(path_to_folder):
+    """
+    get Even Vizion pictures
+    """
+    path_to_folder_with_pictures_ = os.listdir(path_to_folder)
+    pictures_original_pathes = [path_to_folder + "/" + f for f in path_to_folder_with_pictures_ if
+                                (f.endswith('.png') or f.endswith('.jpg'))]
+    pictures_original_pathes.sort()
+    return pictures_original_pathes
+
+
+def draw_video():
+    """
+    create two pictures comparison
+    """
+    cap = cv2.VideoCapture(args.path_to_original_video)
+    _, _ = cap.read()
+    pictures_evenvizion_pathes = get_pictures(args.path_to_evenvizion_result_frames)
+    i = 1
+    for evenvizion_picture_path in pictures_evenvizion_pathes:
+        i += 1
+        _, original_picture_path = cap.read()
+        image_a = image_design(original_picture_path, "Original")
+        image_b = image_design(evenvizion_picture_path, "EvenVizion")
+        logo = cv2.imread(args.path_to_logo)
+        logo = imutils.resize(logo, 64)
+        image_b[-logo.shape[0]:, -logo.shape[1]:, :] = logo
+        image_b = create_border(image_b)
+        result = np.concatenate((image_a, image_b), axis=1)
+        cv2.imwrite("{}/{}.png".format(args.save_folder, "%06d" % i), result)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='custom arguments')
+    parser.add_argument('--path_to_original_video',
+                        default="test_video/test_video.mp4")
+    parser.add_argument('--path_to_evenvizion_result_frames',
+                        help="path to folder with frames after evenvizion_visualization.py",
+                        default="experiment/test_video_processing/visualize camera stabilization")
+    parser.add_argument('--path_to_logo',
+                        default="oxagile_logo.png")
+    parser.add_argument('--experiment_folder', help="folder to save experiment result",
+                        default="experiment/test_video_processing")
+    parser.add_argument('--experiment_name', type=str, default="original_video_with_EvenVizion")
+    args = parser.parse_args()
+    args.save_folder = args.experiment_folder + '/{}'.format(args.experiment_name)
+    if not os.path.exists(args.save_folder):
+        os.mkdir(args.save_folder)
+    draw_video()
---
+++ evenvizion_visualization.py
@@ -0,0 +1,147 @@
+"""
+to visualize EvenVizion on grey background
+"""
+import argparse
+import json
+import os
+from copy import deepcopy
+
+import cv2
+import imutils
+import numpy as np
+
+
+def homography_transformation(vector, matrix_H):
+    """
+    applying homography transformation with matrix H
+    """
+    new_vector = np.dot(matrix_H, vector)
+    return new_vector / new_vector[-1]
+
+
+def brightness_decrease(background_result, background):
+    """
+    decrease brightness of previous frame
+    """
+    hsv = cv2.cvtColor(background_result, cv2.COLOR_BGR2HSV)
+    hsv_background = cv2.cvtColor(background, cv2.COLOR_BGR2HSV)
+    v_value = np.array(deepcopy(hsv_background[..., 2]))
+    hsv_background[..., 2] = np.where((v_value - 2) >= 254, 0, v_value - 2)
+    for i, _ in enumerate(hsv):
+        for j in range(0, len(hsv[i])):
+            if hsv[i][j][2] >= 2:
+                hsv[i][j][2] -= 2
+            else:
+                hsv[i][j] = [222, 12.35, 31.76]
+    return hsv, hsv_background
+
+
+def reformat_homography_dict(gomography_dict):
+    """
+    get matrix superposition for each frames
+    """
+    del gomography_dict["resize_info"]
+    coner_1 = [0, 0, 1]
+    x_corner = []
+    y_corner = []
+    matrix_H_next = None
+    matrix_H_first = True
+    matrix_H_superposition_dict = {}
+    for frame_no, frame_H in gomography_dict.items():
+        if frame_H["H"] is not None:
+            if matrix_H_first:
+                matrix_H_next = frame_H["H"]
+                matrix_H_first = False
+            else:
+                matrix_H_next = np.dot(frame_H["H"], matrix_H_next)
+        matrix_H_superposition_dict[frame_no] = matrix_H_next
+        x_corner.append(homography_transformation(coner_1, matrix_H_next)[0])
+        y_corner.append(homography_transformation(coner_1, matrix_H_next)[1])
+    max_x = int(np.max(x_corner))
+    min_x = int(np.min(x_corner))
+    max_y = int(np.max(y_corner))
+    min_y = int(np.min(y_corner))
+    return matrix_H_superposition_dict, max_x, min_x, max_y, min_y
+
+
+def frame_transformation(background, min_x, x_offset, min_y, y_offset, color_shape):
+    """
+    transform frame according matrix superposition
+    """
+    background_result = deepcopy(background)
+    start_point = (np.abs(min_x) + x_offset, np.abs(min_y) + y_offset)
+    end_point = (np.abs(min_x) + x_offset, np.abs(min_y) + y_offset + color_shape[0])
+    background_result = cv2.line(background_result, start_point,
+                                 end_point, color=[255, 255, 255], thickness=1)
+
+    start_point = (np.abs(min_x) + x_offset, np.abs(min_y) + y_offset)
+    end_point = (np.abs(min_x) + x_offset + color_shape[1], np.abs(min_y) + y_offset)
+    background_result = cv2.line(background_result, start_point,
+                                 end_point, color=[255, 255, 255], thickness=1)
+
+    start_point = (np.abs(min_x) + x_offset + color_shape[1], np.abs(min_y) + y_offset)
+    end_point = (np.abs(min_x) + x_offset + color_shape[1],
+                 np.abs(min_y) + y_offset + color_shape[0])
+    background_result = cv2.line(background_result, start_point,
+                                 end_point, color=[255, 255, 255], thickness=1)
+
+    start_point = (np.abs(min_x) + x_offset + color_shape[1],
+                   np.abs(min_y) + y_offset + color_shape[0])
+    end_point = (np.abs(min_x) + x_offset, np.abs(min_y) + y_offset + color_shape[0])
+    background_result = cv2.line(background_result, start_point,
+                                 end_point, color=[255, 255, 255], thickness=1)
+    return background_result
+
+
+def stabilization_view():
+    """
+    visualize components works
+    """
+    with open(args.path_to_homography_dict, "r") as curr_json:
+        homography_dict = json.load(curr_json)
+        width = homography_dict["resize_info"]["w"]
+    cap = cv2.VideoCapture(args.path_to_original_video)
+    _, image_template = cap.read()
+    image_template = imutils.resize(image_template, width=width)
+    homography_superposition_dict, max_x, min_x, max_y, min_y = \
+        reformat_homography_dict(homography_dict)
+    panorama_shape = [int(np.abs(min_y) + max_y + image_template.shape[0] + 10),
+                      int(np.abs(min_x) + max_x + image_template.shape[1] + 10)]
+
+    background = np.full((panorama_shape[0], panorama_shape[1], 3), 0, dtype=np.uint8)
+    background[np.abs(min_y):np.abs(min_y) + image_template.shape[0],
+               np.abs(min_x):np.abs(min_x) + image_template.shape[1]] = image_template
+    color_shape = [image_template.shape[0], image_template.shape[1]]
+    for (frame_no, frame_H) in homography_superposition_dict.items():
+        _, image_new = cap.read()
+        image_new = imutils.resize(image_new, width=width)
+        transform_vector = (np.dot(frame_H, [0, 0, 1]))
+        x_offset = int(transform_vector[0] / transform_vector[2])
+        y_offset = int(transform_vector[1] / transform_vector[2])
+        background[np.abs(min_y) + y_offset:np.abs(min_y) + y_offset + color_shape[0],
+                   np.abs(min_x) + x_offset:np.abs(min_x) + x_offset + color_shape[1]] = image_new
+        background_result = frame_transformation(background, min_x,
+                                                 x_offset, min_y, y_offset, color_shape)
+        hsv, hsv_background = brightness_decrease(background_result,
+                                                  background)
+        background_result = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        background = cv2.cvtColor(hsv_background, cv2.COLOR_HSV2BGR)
+        cv2.imwrite("{}/img{}.png".format(args.save_folder,
+                                          "%06d" % int(frame_no)), background_result)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='custom arguments without using Sacred library ')
+    parser.add_argument('--path_to_homography_dict', help="path to homography dict",
+                        default="experiment/test_video_processing/dict_with_homography_matrix.json")
+    parser.add_argument('--path_to_original_video',
+                        default="test_video/test_video.mp4")
+    parser.add_argument('--experiment_name',
+                        default="visualize_camera_stabilization")
+    parser.add_argument('--experiment_folder', help="folder to save experiment result",
+                        default="experiment/test_video_processing")
+    args = parser.parse_args()
+    args.save_folder = args.experiment_folder + "/" + args.experiment_name
+    if not os.path.exists(args.save_folder):
+        os.makedirs(args.save_folder)
+    stabilization_view()
