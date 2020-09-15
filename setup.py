d244b38cb8d726e36c14ea4cd62ceebf3eaf1059
dianaK-oss <diana.kulich@oxagile.com>
2020-09-15 11:08:06 +0300

    change setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="EvenVizion-pkg-AIHunters",
+    name="evenvizion",
     version="0.9",
     author="AIHunters",
     author_email="oss@aihunters.com",
