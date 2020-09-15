30e7df6ffb16440750cce9fbbeb2388f65c50406
dianaK-oss <diana.kulich@oxagile.com>
2020-09-15 15:32:13 +0300

    add new requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="evenvizion",
-    version="0.9.2",
+    version="0.9.3",
     author="AIHunters",
     author_email="oss@aihunters.com",
     description="EvenVizion - is a video-based camera localization component",
