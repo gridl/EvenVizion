6226c4cd4af04df521096f5b61df8a4834db21c6
dianaK-oss <diana.kulich@oxagile.com>
2020-09-15 10:50:03 +0300

    add new email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="EvenVizion-pkg-Oxagile_Rnd",
     version="0.9",
     author="AIHunters",
-    author_email="oss@aihunters",
+    author_email="oss@aihunters.com",
     description="EvenVizion - is a video-based camera localization component",
     url="https://github.com/AIHunters/EvenVizion",
     packages=setuptools.find_packages(),
